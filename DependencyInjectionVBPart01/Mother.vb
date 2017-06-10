9c3543287316acbb81a1b3ab5308745031cd9122
Andre Obelink <andre@obelink.com>
2017-06-10 23:38:51 +0200

    Removed code what was commented out

--- DependencyInjectionVBPart01/Mother.vb
+++ DependencyInjectionVBPart01/Mother.vb
@@ -2,8 +2,6 @@
 
     Public Function GetDrinkService() As IDrinkService
         Return New MilkDrinkService()
-        'Return New OrangeJuiceDrinkService()
-        'Return New AppleJuiceDrinkService()
     End Function
 
 End Class
